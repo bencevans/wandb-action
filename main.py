c8aef4a313367892869880e17ce892fc5d9a6418
Ben Evans <ben@bluechimp.io>
2022-11-14 14:55:08 +0000

    format

--- main.py
+++ main.py
@@ -4,74 +4,130 @@ import random
 import os
 from github import Github
 
-wandb.login()
-api = wandb.Api()
 
-github = Github(os.environ["GITHUB_TOKEN"])
-
-ref_current = os.environ["GITHUB_SHA"]
-ref_previous = os.environ["GITHUB_BASE_REF"]
-
-print("Current ref: ", ref_current)
-print("Previous ref: ", ref_previous)
-
-runs_current = api.runs(
-    path="bencevans/wandb-action",
-    filters={"commit": ref_current},
-)
-run_current = runs_current[0] if len(runs_current) > 0 else None
-
-runs_previous = api.runs(
-    path="bencevans/wandb-action",
-    filters={"commit": ref_previous},
-)
-run_previous = runs_previous[0] if len(runs_previous) > 0 else None
-
-if run_current is None:
-    print("No current run")
-
-if run_previous is None:
-    print("No previous run")
-
-all_keys = set()
-if run_current is not None:
-    for key in run_current.summary.keys():
-        all_keys.add(key)
-
-if run_previous is not None:
-    for key in run_previous.summary.keys():
-        all_keys.add(key)
-
-github_commit = github.get_repo("bencevans/wandb-action").get_commit(ref_current)
-
-comment = github_commit.create_comment(
+def get_run(api: wandb.Api, path: str, sha: str) -> wandb.apis.public.Run:
     """
-# Summary
+    Retrieves a run from wandb given a path and commit sha
+    """
 
-Metric | Result
------- | ------
-{}
-
-{}
-""".format(
-        "\n".join([f"{key} | {run_current.summary[key]}" for key in sorted(all_keys)]),
-        "https://app.wandb.ai/bencevans/wandb-action/runs/{}".format(run_current.id),
+    api.runs(
+        path=wandb_path,
+        filters={"commit": ref_current},
     )
-)
+    run_current = runs_current[0] if len(runs_current) > 0 else None
 
-print("✍️ Written commit comment: ", comment.html_url)
 
-# print("Key | Previous | Current")
-# print("--- | --- | ---")
-# for key in sorted(list(all_keys)):
-#     previous = (
-#         run_previous.summary[key]
-#         if run_previous is not None and key in run_previous.summary
-#         else ""
-#     )
-#     current = (
-#         run_current.summary[key]
-#         if run_current is not None and key in run_current.summary
-#         else ""
-#     )
-#     print(f"{key} | {previous} | {current}")
+def format_commit_message(run: wandb.apis.public.Run) -> str:
+    """
+    Formats a commit message given a wandb run
+    """
+
+    return """
+    # WandB Summary
+
+    Metric | Result
+    ------ | ------
+    {}
+
+    {}
+    """.format(
+        "\n".join(
+            [
+                f"{key} | {run_current.summary[key]}"
+                for key in sorted(run.summary.keys())
+            ]
+        ),
+        run.url,
+    )
+
+
+def format_pr_message(
+    prev_run: wandb.apis.public.Run, curr_run: wandb.apis.public.Run
+) -> str:
+    """
+    Formats a PR message given two wandb runs
+    """
+    return """
+    # WandB Summary
+
+    Metric | Previous | Current
+    ------ | -------- | -------
+    {}
+
+    - [Previous Run]({prev_run_url})
+    - [Current Run]({curr_run_url})
+    """.format(
+        "\n".join(
+            [
+                f"{key} | {prev_run.summary[key]} | {curr_run.summary[key]}"
+                for key in sorted(
+                    set(prev_run.summary.keys()).union(set(curr_run.summary.keys()))
+                )
+            ]
+        ),
+        prev_run_url=prev_run.url,
+        curr_run_url=curr_run.url,
+    )
+
+
+if __name__ == "__main__":
+    wandb.login()
+    wandb_api = wandb.Api()
+
+    github_token = os.environ["GITHUB_TOKEN"]
+    github_repo = os.environ["GITHUB_REPOSITORY"]
+
+    ref_current = os.environ["GITHUB_SHA"]
+    ref_previous = os.environ["GITHUB_BASE_REF"]
+
+    wandb_entity = (
+        os.environ["WANDB_ENTITY"]
+        if "WANDB_ENTITY" in os.environ
+        else os.environ["GITHUB_REPOSITORY"].split("/")[0]
+    )
+    wandb_project = (
+        os.environ["WANDB_PROJECT"]
+        if "WANDB_PROJECT" in os.environ
+        else os.environ["GITHUB_REPOSITORY"].split("/")[1]
+    )
+    wandb_path = wandb_entity + "/" + wandb_project
+
+    print("Current ref: ", ref_current)
+    print("Previous ref: ", ref_previous)
+
+    github_api = Github(github_token)
+    github_api_repo = github.get_repo(github_repo)
+
+    runs_current = get_run(wandb_api, wandb_path, ref_current)
+    runs_previous = get_run(wandb_api, wandb_path, ref_previous)
+
+    if runs_current is None:
+        print(f"⚠️ No run found for current ref {ref_current}")
+        exit(1)
+
+    if runs_previous is None:
+        print(f"⚠️ No run found for previous ref {ref_previous}")
+
+    all_keys = set()
+    if run_current is not None:
+        for key in run_current.summary.keys():
+            all_keys.add(key)
+
+    if run_previous is not None:
+        for key in run_previous.summary.keys():
+            all_keys.add(key)
+
+    print(
+        "✍️ Written commit comment: ",
+        github_api_repo.get_commit(ref_current)
+        .create_comment(format_commit_message(run_current))
+        .html_url,
+    )
+
+    if run_previous is not None:
+        print(
+            "✍️ Written pull request comment: ",
+            github_api_repo.get_issue(run_previous.pull_request.number)
+            .create_issue_comment(format_pr_message(run_previous))
+            .html_url,
+        )
