6dae9d2cf2c5b44a600fb7f0823c688ffc1f344f
Ben Evans <ben@bluechimp.io>
2022-11-14 15:47:00 +0000

    format

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ GitHub Action for reporting Weights & Biases metrics.
 """
 
 import os
-import wandb
-import random
-import os
+import sys
+from typing import Optional
 from github import Github
+import wandb
 
 
-def get_run(api: wandb.Api, wandb_path: str, sha: str) -> wandb.apis.public.Run:
+def get_run(api: wandb.Api, path: str, sha: str) -> wandb.apis.public.Run:
     """
     Retrieves a run from wandb given a path and commit sha
     """
 
     runs = api.runs(
-        path=wandb_path,
+        path=path,
         filters={"commit": sha},
     )
     return runs[0] if len(runs) > 0 else None
@@ -26,52 +26,62 @@ def format_commit_message(run: wandb.apis.public.Run) -> str:
     Formats a commit message given a wandb run
     """
 
-    return """
-    # WandB Summary
-
-    Metric | Result
-    ------ | ------
-    {}
-
-    {}
-    """.format(
-        "\n".join(
-            [
-                f"{key} | {run.summary[key]}"
-                for key in sorted(run.summary.keys())
-            ]
-        ),
-        run.url,
+    return "\n".join(
+        [
+            "# WandB Summary",
+            "",
+            "Metric | Value",
+            "------ | -----",
+            "{}".format(
+                "\n".join(
+                    [
+                        f"{key} | {run.summary[key]}"
+                        for key in sorted(run.summary.keys())
+                    ]
+                ),
+            ),
+            "",
+            f"- [Open on WandB]({run.url})",
+            "",
+        ]
     )
 
 
 def format_pr_message(
-    prev_run: wandb.apis.public.Run, curr_run: wandb.apis.public.Run
+    prev_run: Optional[wandb.apis.public.Run],
+    prev_ref: str,
+    curr_run: wandb.apis.public.Run,
+    curr_ref: str,
 ) -> str:
     """
     Formats a PR message given two wandb runs
     """
-    return """
-    # WandB Summary
+    keys = sorted(set(prev_run.summary.keys()).union(set(curr_run.summary.keys())))
 
-    Metric | Previous | Current
-    ------ | -------- | -------
-    {}
+    message = [
+        "# WandB Summary",
+        "",
+        f"{prev_ref}...{curr_ref}",
+        "",
+        "Metric | Previous | Current",
+        "------ | -------- | -------",
+    ]
 
-    - [Previous Run]({prev_run_url})
-    - [Current Run]({curr_run_url})
-    """.format(
-        "\n".join(
-            [
-                f"{key} | {prev_run.summary[key]} | {curr_run.summary[key]}"
-                for key in sorted(
-                    set(prev_run.summary.keys()).union(set(curr_run.summary.keys()))
-                )
-            ]
-        ),
-        prev_run_url=prev_run.url,
-        curr_run_url=curr_run.url,
-    )
+    for key in keys:
+        prev_value = (
+            prev_run.summary[key]
+            if prev_run is not None and key in prev_run.summary
+            else "-"
+        )
+        curr_value = curr_run.summary[key] if key in curr_run.summary else "-"
+        message.append(f"{key} | {prev_value} | {curr_value}")
+
+    message.append("")
+    if prev_run is not None:
+        message.append(f"- [Open Previous on WandB]({prev_run.url})")
+    message.append(f"- [Open Current on WandB]({curr_run.url})")
+    message.append("")
+    return "\n".join(message)
 
 
 if __name__ == "__main__":
@@ -81,8 +91,8 @@ if __name__ == "__main__":
     github_token = os.environ["GITHUB_TOKEN"]
     github_repo = os.environ["GITHUB_REPOSITORY"]
 
-    ref_current = os.environ["GITHUB_SHA"]
-    ref_previous = os.environ["GITHUB_BASE_REF"]
+    curr_ref = os.environ["GITHUB_SHA"]
+    prev_ref = os.environ["GITHUB_BASE_REF"]
 
     is_pull_request = os.environ["GITHUB_EVENT_NAME"] == "pull_request"
 
@@ -98,33 +108,32 @@ if __name__ == "__main__":
     )
     wandb_path = wandb_entity + "/" + wandb_project
 
-    print("Current ref: ", ref_current)
-    print("Previous ref: ", ref_previous)
+    print(f"Current ref: '{curr_ref}'")
+    print(f"Previous ref: '{prev_ref}'")
 
     github_api = Github(github_token)
     github_api_repo = github_api.get_repo(github_repo)
 
-    run_curr = get_run(wandb_api, wandb_path, ref_current)
-    run_prev = get_run(wandb_api, wandb_path, ref_previous)
+    curr_run = get_run(wandb_api, wandb_path, curr_ref)
 
-    if run_curr is None:
-        print(f"⚠️ No run found for current ref {ref_current}")
-        exit(1)
+    if curr_run is None:
+        print(f"⚠️ No WandB run found for current ref '{curr_ref}'")
+        sys.exit(1)
 
-    if run_prev is None:
-        print(f"⚠️ No run found for previous ref {ref_previous}")
+    if is_pull_request:
+        prev_run = get_run(wandb_api, wandb_path, prev_ref)
 
-    print(
-        "✍️ Written commit comment: ",
-        github_api_repo.get_commit(ref_current)
-        .create_comment(format_commit_message(run_curr))
-        .html_url,
-    )
-
-    if run_prev is not None:
+        # TODO: Comment on PR rather than commit
         print(
-            "TODO: ✍️ Written pull request comment: ",
-            # github_api_repo.get_issue(run_prev.pull_request.number)
-            # .create_issue_comment(format_pr_message(run_prev))
-            # .html_url,
+            "✍️ Written pull request comment: ",
+            github_api_repo.get_commit(curr_ref)
+            .create_comment(format_pr_message(prev_run, prev_ref, curr_run, curr_ref))
+            .html_url,
+        )
+    else:
+        print(
+            "✍️ Written commit comment: ",
+            github_api_repo.get_commit(curr_ref)
+            .create_comment(format_commit_message(curr_run))
+            .html_url,
         )
